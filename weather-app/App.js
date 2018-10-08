b011e03822fd4c1821cc28ba619926c11eacfbec
JongGyun Park <jonggyuni@gmail.com>
2018-10-08 23:20:46 +0900

    add gitignore

--- weather-app/App.js
+++ weather-app/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import Weather from "./Weather";
+import API_KEY from './APIkey';
 
 export default class App extends React.Component {
   state = {
-    isLoaded: true
+    isLoaded: false,
+    error: null
   };
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: error
+        });
+      }
+    );
+  }
+  _getWeather = (lat, lon) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      
+    })
+  }
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true}/>
@@ -16,6 +38,7 @@ export default class App extends React.Component {
           ) : (
           <View style={styles.loading}>
             <Text style={styles.loadingText}>Getting the WeatherApp</Text>
+            {error ? <Text style={styles.error}>{error}</Text> : null}
           </View>
         )}
       </View>
@@ -28,6 +51,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'center',
+  },
+  error: {
+    color: 'red',
+    backgroundColor: 'transparent',
+    marginBottom: 40
   },
   loading:{
     flex:1,

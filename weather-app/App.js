d544de9bda949ca95ad3ec2c1b6d1041680eaad2
JongGyun Park <jonggyuni@gmail.com>
2018-10-08 22:44:45 +0900

    create weather app

--- weather-app/App.js
+++ weather-app/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import Weather from "./Weather";
 
 export default class App extends React.Component {
+  state = {
+    isLoaded: true
+  };
   render() {
+    const { isLoaded } = this.state;
     return (
       <View style={styles.container}>
-        <Text>Hello!! Test Text!!! really?</Text>
+        <StatusBar hidden={true}/>
+        {isLoaded ? (
+          <Weather/>
+          ) : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the WeatherApp</Text>
+          </View>
+        )}
       </View>
     );
   }
@@ -15,7 +27,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  loading:{
+    flex:1,
+    backgroundColor: '#FDF6AA',
+    justifyContent: 'flex-end',
+    paddingLeft: 25
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 100
+  }
 });
